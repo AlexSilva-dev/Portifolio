044836f5a76c3703f4111e852f76d50580290e6a
AlexSilva-dev <alexdsilva.dev@gmail.com>
2024-02-22 21:29:00 -0300

    deploy

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-    
+    <div class="container my-5 pt-5">
+        <Banner></Banner>
+        <AboutMe class="mt-300" id="about-me"></AboutMe>
+        <Technology class="mt-300"></Technology>
+        <Contact class="mt-300"></Contact>
+    </div>
 </template>
+
+<style>
+body {
+    padding-bottom: 30%;
+}
+.mt-300 {
+    margin-top: 300px;
+}
+</style>
+
